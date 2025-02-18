d1092dae56d0bfaf8fb7526b26e35c40dcd27546
Rostislav Alekseev <mr.rostik.01@gmail.com>
2025-02-18 20:32:15 +0200

    header component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,10 @@
 <script setup lang="ts">
-import { RouterView, RouterLink } from 'vue-router'
+import { RouterView } from 'vue-router'
+import AppHeader from '@/components/AppHeader.vue'
 </script>
 
 <template>
-  <header>
-    <h1>Тестовое задание</h1>
-    <nav>
-      <RouterLink to="/">Главная</RouterLink>
-      <RouterLink to="/uikit">Компоненты</RouterLink>
-    </nav>
-  </header>
-
+  <AppHeader />
   <RouterView />
 </template>
 
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <h1>Тестовое задание</h1>
+    <nav>
+      <RouterLink to="/">Главная</RouterLink>
+      <RouterLink to="/uikit">Компоненты</RouterLink>
+    </nav>
+  </header>
+</template>
+
+<style scoped lang="scss">
+header {
+  background-color: #000;
+  height: 100px;
+  color: #fff;
+  padding: 10px;
+  display: flex;
+  justify-content: space-around;
+  align-items: flex-end;
+
+  nav {
+    display: flex;
+    gap: 20px;
+
+    a {
+      text-decoration: none;
+      color: #fff;
+      transition: 0.2s;
+    }
+    a:hover {
+      opacity: 0.8;
+      transition: 0.2s;
+    }
+
+    .router-link-active {
+      font-weight: bold;
+    }
+  }
+}
+</style>
