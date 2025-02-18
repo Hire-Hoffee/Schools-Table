72bb6951011ec319d0bf61d913fa9b7f437ad4bb
Rostislav Alekseev <mr.rostik.01@gmail.com>
2025-02-18 18:35:53 +0200

    initial project commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterView, RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <h1>Тестовое задание</h1>
+    <nav>
+      <RouterLink to="/">Главная</RouterLink>
+      <RouterLink to="/uikit">Компоненты</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
