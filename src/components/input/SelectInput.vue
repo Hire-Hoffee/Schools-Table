08044e46aab66482dbe08a1dd6b864719a628140
Rostislav Alekseev <mr.rostik.01@gmail.com>
2025-02-19 14:53:32 +0200

    select input component

---
+++ src/components/input/SelectInput.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="select-wrapper">
+    <select>
+      <option value="all" selected>Все виды</option>
+      <option value="Permanent">Постоянное</option>
+      <option value="Unknown">Не определен</option>
+    </select>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.select-wrapper {
+  width: 100%;
+  position: relative;
+
+  &::after {
+    content: url('@/assets/images/arrow_down.svg');
+    position: absolute;
+    top: 50%;
+    right: 20px;
+    transform: translateY(-50%);
+    pointer-events: none;
+    transition: 0.2s;
+  }
+
+  &:focus-within::after {
+    transform: translateY(-50%) rotate(180deg);
+    transition: 0.2s;
+  }
+
+  select {
+    width: inherit;
+    padding: 18px;
+    border-radius: 10px;
+    border: 1px solid #eee;
+    appearance: none;
+  }
+
+  select:focus {
+    outline: 1px solid #ddd;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,26 @@
 <script setup lang="ts">
 import EduTable from '@/components/table/EduTable.vue'
+import SelectInput from '@/components/input/SelectInput.vue'
 </script>
 
 <template>
   <h1>Таблица учреждений</h1>
+  <div class="inputs-block">
+    <SelectInput />
+    <SelectInput />
+    <SelectInput />
+  </div>
   <EduTable />
 </template>
 
 <style scoped lang="scss">
 h1 {
   font-weight: bold;
+}
+
+.inputs-block {
+  display: flex;
+  gap: 16px;
+  margin-top: 24px;
 }
 </style>
